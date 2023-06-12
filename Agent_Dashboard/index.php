ef89c8a2aadcabd0d8c09756c5cff847c832fce0
Joseph Ogachi <91715300+josephogachi@users.noreply.github.com>
2023-06-12 22:09:02 +0300

    index.php

---
+++ Agent_Dashboard/index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="agent.css">
+    <title>Agents Dasboard</title>
+</head>
+<body>
+
+    <div class="sidemenu">
+        <div class="title">
+            <h1>Agent</h1></div>
+        <ul class="menu">
+            <li class="active">
+                <a href="#">
+                    <img src="assets/user.png" alt="#">
+                    <span>Details</span>
+                </a>
+
+            </li>
+            
+            <li>
+                <a href="#">
+                    <img src="assets/user.png" alt="#">
+                    <span>Lender</span>
+                </a>
+
+            </li>
+        
+            <li>
+                <a href="#">
+                    <img src="assets/users-alt.png" alt="#">
+                    <span>Customer</span>
+                </a>
+
+            </li>
+            
+            <li>
+                <a href="#">
+                    <img src="assets/sack-dollar.png" alt="#">
+                    <span>Commission</span>
+                </a>
+
+            </li>
+            
+            <li>
+                <a href="#">
+                    <img src="assets/earnings.png" alt="#">
+                    <span>Reports</span>
+                </a>
+
+            </li>
+            
+            <li class="logout">
+                <a href="#">
+                    <img src="assets/dashboard.png" alt="#">
+                    <span>Logout</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="content">
+        <div class="header-wrapper">
+            <div class="header-title">
+                <h2>Dashboard</h2>
+                <span>Hi Ogachi</span>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <div class="cards-container">
+        <p>Customer Transactions</p>
+        <div class="cards-wrapper">
+            <div class="cards">
+                <div class="box">
+
+                <div class="icon-class">
+                </div>
+
+                <div class="Balance">
+                    <input type="text" placeholder="500,000.00">
+                    <p> Account Balance</p>
+                    </div>
+                   
+
+              </div>
+            </div>
+        </div>
+        <div class="cards-wrapper">
+            <div class="cards">
+                <div class="box">
+
+                <div class="icon-class">
+                   
+                </div>
+
+                <div class="Balance">
+                    <input type="text" placeholder="+254 7...">
+                    <p> Customer MpesaNo.</p>
+                    </div>
+                   
+
+              </div>
+            </div>
+        </div>
+        <div class="cards-wrapper">
+            <div class="cards">
+                <div class="box">
+
+                <div class="icon-class">
+                </div>
+
+                <div class="Balance">
+                    <input type="text" placeholder="Ksh..">
+                    <p> Amount to LOAN</p><br>
+                    <a href="#" class="btn"> LOAN  MONEY  </a>
+                    </div>
+                   
+
+              </div>
+            </div>
+        </div>
+        <div class="cards-wrapper">
+            <div class="cards">
+                <div class="box">
+
+                <div class="icon-class">
+                    <img src="assets/sack-dollar.png" alt="">
+                </div>
+
+                <div class="Balance">
+                    <input type="text" placeholder="Ksh...">
+                    <p> My Commission</p>
+                    </div>
+                   
+
+              </div>
+            </div>
+        </div>
+        
+    </div><br><br><br><br><br><br><br><br><br>
+    
+    
+    <div class="cards-container2">
+        <p>Lender Transactions</p>
+        <div class="cards-wrapper2">
+            
+            <div class="cards">
+                <div class="box">
+
+                <div class="icon-class">
+                </div>
+
+                <div class="Balance">
+                    <input type="text" placeholder="Ksh..">
+                    <p> Borrowed Money</p>
+                    </div>
+                   
+
+              </div>
+            </div>
+        </div>
+        <div class="cards-wrapper2">
+            
+            <div class="cards">
+                <div class="box">
+
+                <div class="icon-class">
+                </div>
+
+                <div class="Balance">
+                    <input type="text" placeholder="+254 7...">
+                    <p> Lender MpesaNo.</p>
+                    </div>
+                   
+
+              </div>
+            </div>
+        </div>
+           <div class="cards-wrapper2">
+            
+             <div class="cards">
+                <div class="box">
+
+                    <div class="Balance">
+                        <input type="text" placeholder="Ksh..">
+                        <p> Amount to Repay</p><br>
+                        <a href="#" class="btn">SEND MONEY  </a>
+                        </div>
+                  </div>
+              </div>
+        </div>
+
+        <a href="#" class="btn">WITHDRAW MONEY  </a>
+
+        </div>
+        <br><br><br><br><br><br><br>
+
+        <div class="content-2">
+            <div class="money-returned">
+                <div class="title">
+                    <p>Loans Summary</p>
+                    
+                </div>
+                <div class="table">
+                    <table>
+                    <tr>
+                        <th>Customer</th>
+                        <th>Loan</th>
+                        <th>Interest</th>
+                        <th>Amount </th>
+                        <th>Status</th>
+                        <th>Options</th>
+                    </tr>
+                    <tr>
+                        <td>Jose</td>
+                        <td>22,000.00</td>
+                        <td>1400</td>
+                        <td>23,400.00</td>
+                        <td>Payed</td>
+                        <td><a href="#" class="btn">View</a></td>
+                    </tr>
+                    <tr>
+                        <td>Erick</td>
+                        <td>5,000.00</td>
+                        <td>400</td>
+                        <td>5,400.00</td>
+                        <td>Payed</td>
+                        <td><a href="#" class="btn">View</a></td>
+                    </tr>
+                    <tr>
+                        <td>Pascalia</td>
+                        <td>10,000.00</td>
+                        <td>1,000</td>
+                        <td>11,000.00</td>
+                        <td>Partially paid</td>
+                        <td><a href="#" class="btn">View</a></td>
+                    </tr>
+                    <var><tr>
+                        <td>Mary</td>
+                        <td>14,000.00</td>
+                        <td>1200</td>
+                        <td>15,200.00</td>
+                        <td>Not Payeds</td>
+                        <td><a href="#" class="btn">View</a></td>
+                    </tr></var>
+                </table>
+                </div>
+                    
+                   
+                </table>
+           
+        
+    
+</body>
+</html>
